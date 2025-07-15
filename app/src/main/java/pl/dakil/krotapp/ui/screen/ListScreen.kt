08732c83b637d2a150d061bfd24d31f12d0de3ac
palinkiewicz <pogromca.ap@gmail.com>
2025-07-15 21:39:38 +0200

    Add screens UI

--- app/src/main/java/pl/dakil/krotapp/ui/screen/ListScreen.kt
+++ app/src/main/java/pl/dakil/krotapp/ui/screen/ListScreen.kt
@@ -1,12 +1,75 @@
 package pl.dakil.krotapp.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pl.dakil.krotapp.R
 import pl.dakil.krotapp.viewmodel.ItemsViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen(
     itemsViewModel: ItemsViewModel,
     onNavigateBack: () -> Unit
 ) {
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { onNavigateBack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(
+                                R.string.back
+                            )
+                        )
+                    }
+                },
+                title = { Text(text = stringResource(R.string.list_title)) }
+            )
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            itemsIndexed(itemsViewModel.filteredItems.value) { index, item ->
+                Card {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Column {
+                            Text(text = item.name, style = MaterialTheme.typography.titleSmall)
+                            Text(text = item.index, style = MaterialTheme.typography.labelSmall)
+                        }
+                        Text(text = "${item.amount} ${item.unit}", style = MaterialTheme.typography.bodyLarge, modifier = Modifier.padding(start = 12.dp))
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/pl/dakil/krotapp/ui/screen/SearchScreen.kt
+++ app/src/main/java/pl/dakil/krotapp/ui/screen/SearchScreen.kt
@@ -1,10 +1,18 @@
 package pl.dakil.krotapp.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,7 +22,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import pl.dakil.krotapp.R
 import pl.dakil.krotapp.viewmodel.ItemsViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,39 +41,61 @@ fun SearchScreen(
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Stany magazynowe") })
+            TopAppBar(title = { Text(text = stringResource(R.string.search_title)) })
         }
-    ) {
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = { expanded = !expanded }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues).padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            OutlinedTextField(
-                value = selectedMG,
-                onValueChange = {},
-                readOnly = true,
-                label = { Text("MG Number") },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded) },
-                modifier = Modifier
-                    .menuAnchor()
-                    .fillMaxWidth()
-            )
-
-            ExposedDropdownMenu(
+            ExposedDropdownMenuBox(
                 expanded = expanded,
-                onDismissRequest = { expanded = false }
+                onExpandedChange = { expanded = !expanded },
             ) {
-                itemsViewModel.storages.value.forEach { storage ->
-                    DropdownMenuItem(
-                        text = { Text(storage.mg) },
-                        onClick = {
-                            selectedMG = storage.mg
-                            expanded = false
-                        }
-                    )
+                OutlinedTextField(
+                    value = selectedMG,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = { Text(stringResource(R.string.mg_number)) },
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded) },
+                    modifier = Modifier
+                        .menuAnchor()
+                        .fillMaxWidth()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    itemsViewModel.storages.value.forEach { storage ->
+                        DropdownMenuItem(
+                            text = { Text(storage.mg) },
+                            onClick = {
+                                selectedMG = storage.mg
+                                expanded = false
+                            }
+                        )
+                    }
                 }
             }
+            OutlinedTextField(
+                value = nameQuery,
+                onValueChange = { nameQuery = it },
+                label = { Text(stringResource(R.string.item_name)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Button(onClick = {
+                itemsViewModel.filterItems(selectedMG, nameQuery)
+                onNavigateToList()
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "",
+                )
+                Text(text = stringResource(R.string.search_button))
+            }
         }
-
     }
 }
